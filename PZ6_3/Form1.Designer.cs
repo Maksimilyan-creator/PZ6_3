fc8876b4fa5eaac15e1d5a940c8ec5689474e1fd
maksimilyan.lamaev.13@inbox.ru <maksimilyan.lamaev.13@inbox.ru>
2023-04-03 12:01:20 +0700

    ?//

--- PZ6_3/Form1.Designer.cs
+++ PZ6_3/Form1.Designer.cs
@@ -35,11 +35,14 @@
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.StudentView)).BeginInit();
             this.SuspendLayout();
             // 
             // StudentView
             // 
+            this.StudentView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.StudentView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.StudentView.Location = new System.Drawing.Point(12, 12);
             this.StudentView.Name = "StudentView";
@@ -63,6 +66,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(137, 23);
             this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label2
             // 
@@ -77,36 +81,68 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "0-20",
+            "21-50",
+            "51-70",
+            "71-100"});
             this.comboBox1.Location = new System.Drawing.Point(243, 317);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(148, 23);
             this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button1.Location = new System.Drawing.Point(175, 359);
+            this.button1.Location = new System.Drawing.Point(143, 359);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(125, 53);
             this.button1.TabIndex = 5;
             this.button1.Text = "Сортировка по алфавиту";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button2.Location = new System.Drawing.Point(306, 359);
+            this.button2.Location = new System.Drawing.Point(274, 359);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(125, 53);
             this.button2.TabIndex = 6;
             this.button2.Text = "Сброс";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(12, 359);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 53);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Вывод \"+ рейтинг\"";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(663, 359);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(125, 53);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Закрыть";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 422);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox1);
@@ -132,5 +168,7 @@
         private ComboBox comboBox1;
         private Button button1;
         private Button button2;
+        private Button button3;
+        private Button button4;
     }
 }
--- PZ6_3/Form1.cs
+++ PZ6_3/Form1.cs
@@ -1,3 +1,9 @@
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
 namespace PZ6_3
 {
     public partial class Form1 : Form
@@ -17,12 +23,69 @@ namespace PZ6_3
             Students.Add(new Student("abd_31", "Кудрявцева", 20, -9));
             Students.Add(new Student("is_vp_31", "Семенова", 45, 20));
             Students.Add(new Student("is_vp_32", "Крылова", 20, -9));
-            StudentView.DataSource = Students;
+            StudentView.DataSource = Students.ToList();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            StudentView.DataSource = Students.Where(u => u.Group == textBox1.Text).ToList();
+            if (textBox1.Text == "")
+            {
+                StudentView.DataSource = Students.ToList();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           switch(comboBox1.SelectedIndex)
+            {
+                case 0: //0-20
+
+                    StudentView.DataSource = Students.Where(u => u.Progress >=0 && u.Progress <=20 ).ToList();
+                    
+
+                    break;
+                case 1: //21-50
+
+                    StudentView.DataSource = Students.Where(u => u.Progress >= 21 && u.Progress <= 50).ToList();
+
+                    break;
+                case 2: //51-70
+
+                    StudentView.DataSource = Students.Where(u => u.Progress >= 51 && u.Progress <= 70).ToList();
+
+                    break;
+                case 3: //71-100
+
+                    StudentView.DataSource = Students.Where(u => u.Progress >= 71 && u.Progress <= 100).ToList();
+
+                    break;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StudentView.DataSource = Students.Where(u => u.Rating >= 0).ToList();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StudentView.DataSource = Students.OrderBy(u => u.Surname).ToList();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StudentView.DataSource = Students.ToList();
         }
     }
 }
